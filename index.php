9d5d4652e533c265fa43c38b3da3e2b945506b6f
Matias Niemela <matias@yearofmoo.com>
2012-05-07 01:37:34 -0400

    demos page content setup

--- index.php
+++ index.php
@@ -1,1 +1,52 @@
-hello
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+<title>Yearofmoo.com - Demos</title> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<link rel="stylesheet" type="text/css" href="./stylesheets/reset.css" />
+<link rel="stylesheet" type="text/css" href="./stylesheets/layout.css" />
+</head> 
+
+<body id="" class=""> 
+  <div id="wrapper"> 
+    <header id="header">
+      <div class="wrap">
+        <a href="/" id="logo">
+          <h1>Yearofmoo</h1>
+        </a>
+        <nav id="main-navigation">
+          <ol>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/plugins">Plugins</a>
+            </li>
+            <li class="last">
+              <a href="http://yearofmoo.com">Return to Yearofmoo.com</a>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </header>
+    <section id="content">
+      <div class="wrap">
+      </div>
+    </section>
+    <footer id="footer">
+      <div class="wrap">
+      </div>
+    </footer>
+  </div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/mootools/1.4.5/mootools-yui-compressed.js"></script>
+<script type="text/javascript" src="./javascripts/MooTools-more.js"></script>
+<script type="text/javascript" src="./javascripts/plugins/cufon/js/cufon.js"></script>
+<script type="text/javascript" src="./fonts/harbara.js"></script>
+<script type="text/javascript" src="./fonts/nevis.js"></script>
+<script type="text/javascript">
+window.addEvent('domready',function() {
+  Cufon.replace('#main-navigation a', { fontFamily: 'Nevis' });
+});
+</script>
+</body>
+</html> 
